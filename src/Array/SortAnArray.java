467f72e3484fa8fcceab849baf6b2353bfc04b4e
Shubham Khare <shubhamkhare60@gmail.com>
2025-08-21 10:58:58 +0530

    sortArray program added +	new file:   bin/Array/SortAnArray.class +	modified:   bin/demo.class +	new file:   src/Array/SortAnArray.java +	modified:   src/demo.java

---
+++ src/Array/SortAnArray.java
@@ -0,0 +1,19 @@
+package Array;
+
+public class SortAnArray {
+    public static void main(String[] args) {
+    int arr[] = {9,4,6};
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0; j<arr.length-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp= arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    
+                }
+            }
+        }
+        for(int num : arr)
+        System.out.print(num + " ");
+}
+}
--- src/demo.java
+++ src/demo.java
@@ -1,14 +1,22 @@
+import java.util.HashSet;
+
 import Strings.reverseString;
 
 public class demo {
     // reverse only last word
     public static void main(String[] args) {
-       String str = "Shubham Khare";
-       String rev = "";
-       for(int i=str.length()-1;i>=0;i--){
-        rev+= str.charAt(i);
-       } 
-    System.out.println(rev);
+       //wap to remove duplicate character
+       String str = "AAABBBBB";
+       HashSet<Character> set = new HashSet<>();
+       for(int i=0;i<str.length();i++)
+       {
+        set.add(str.charAt(i));
+       }
+       for(Character ch : set)
+       {
+        System.out.print(ch);
+       }
+
     }
 
 }
